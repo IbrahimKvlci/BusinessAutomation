4a6ebd6e81cf95d54b85aa156cdf002289324b36
Halil İbrahim Kavalcı <110342285+IbrahimKvlci@users.noreply.github.com>
2023-08-15 21:33:26 +0300

    Added Get Forms

--- WindowsFormsApp/Form1.Designer.cs
+++ WindowsFormsApp/Form1.Designer.cs
@@ -28,19 +28,166 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.btnECommercePanel = new System.Windows.Forms.Button();
+            this.btnSetSalePanel = new System.Windows.Forms.Button();
+            this.btnProductCrudPanel = new System.Windows.Forms.Button();
+            this.btnMainMenuPanel = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pnlMain = new System.Windows.Forms.Panel();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(40)))), ((int)(((byte)(41)))));
+            this.panel2.Controls.Add(this.btnECommercePanel);
+            this.panel2.Controls.Add(this.btnSetSalePanel);
+            this.panel2.Controls.Add(this.btnProductCrudPanel);
+            this.panel2.Controls.Add(this.btnMainMenuPanel);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 111);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(125, 596);
+            this.panel2.TabIndex = 1;
+            // 
+            // btnECommercePanel
+            // 
+            this.btnECommercePanel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnECommercePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnECommercePanel.FlatAppearance.BorderSize = 0;
+            this.btnECommercePanel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnECommercePanel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(246)))), ((int)(((byte)(224)))));
+            this.btnECommercePanel.Image = ((System.Drawing.Image)(resources.GetObject("btnECommercePanel.Image")));
+            this.btnECommercePanel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnECommercePanel.Location = new System.Drawing.Point(0, 210);
+            this.btnECommercePanel.Name = "btnECommercePanel";
+            this.btnECommercePanel.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnECommercePanel.Size = new System.Drawing.Size(125, 70);
+            this.btnECommercePanel.TabIndex = 3;
+            this.btnECommercePanel.Text = "  E-Ticaret İşlemleri";
+            this.btnECommercePanel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnECommercePanel.UseVisualStyleBackColor = true;
+            this.btnECommercePanel.Click += new System.EventHandler(this.btnECommercePanel_Click);
+            // 
+            // btnSetSalePanel
+            // 
+            this.btnSetSalePanel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnSetSalePanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnSetSalePanel.FlatAppearance.BorderSize = 0;
+            this.btnSetSalePanel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSetSalePanel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(246)))), ((int)(((byte)(224)))));
+            this.btnSetSalePanel.Image = ((System.Drawing.Image)(resources.GetObject("btnSetSalePanel.Image")));
+            this.btnSetSalePanel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSetSalePanel.Location = new System.Drawing.Point(0, 140);
+            this.btnSetSalePanel.Name = "btnSetSalePanel";
+            this.btnSetSalePanel.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnSetSalePanel.Size = new System.Drawing.Size(125, 70);
+            this.btnSetSalePanel.TabIndex = 2;
+            this.btnSetSalePanel.Text = "  İndirim/Zam";
+            this.btnSetSalePanel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnSetSalePanel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSetSalePanel.UseVisualStyleBackColor = true;
+            this.btnSetSalePanel.Click += new System.EventHandler(this.btnSetSalePanel_Click);
+            // 
+            // btnProductCrudPanel
+            // 
+            this.btnProductCrudPanel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnProductCrudPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnProductCrudPanel.FlatAppearance.BorderSize = 0;
+            this.btnProductCrudPanel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProductCrudPanel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(246)))), ((int)(((byte)(224)))));
+            this.btnProductCrudPanel.Image = ((System.Drawing.Image)(resources.GetObject("btnProductCrudPanel.Image")));
+            this.btnProductCrudPanel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProductCrudPanel.Location = new System.Drawing.Point(0, 70);
+            this.btnProductCrudPanel.Name = "btnProductCrudPanel";
+            this.btnProductCrudPanel.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnProductCrudPanel.Size = new System.Drawing.Size(125, 70);
+            this.btnProductCrudPanel.TabIndex = 1;
+            this.btnProductCrudPanel.Text = "  Ürün İşlemleri";
+            this.btnProductCrudPanel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnProductCrudPanel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnProductCrudPanel.UseVisualStyleBackColor = true;
+            this.btnProductCrudPanel.Click += new System.EventHandler(this.btnProductCrudPanel_Click);
+            // 
+            // btnMainMenuPanel
+            // 
+            this.btnMainMenuPanel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMainMenuPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.btnMainMenuPanel.FlatAppearance.BorderSize = 0;
+            this.btnMainMenuPanel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMainMenuPanel.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(246)))), ((int)(((byte)(224)))));
+            this.btnMainMenuPanel.Image = ((System.Drawing.Image)(resources.GetObject("btnMainMenuPanel.Image")));
+            this.btnMainMenuPanel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnMainMenuPanel.Location = new System.Drawing.Point(0, 0);
+            this.btnMainMenuPanel.Name = "btnMainMenuPanel";
+            this.btnMainMenuPanel.Padding = new System.Windows.Forms.Padding(5, 0, 0, 0);
+            this.btnMainMenuPanel.Size = new System.Drawing.Size(125, 70);
+            this.btnMainMenuPanel.TabIndex = 0;
+            this.btnMainMenuPanel.Text = "  Ana Menü";
+            this.btnMainMenuPanel.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnMainMenuPanel.UseVisualStyleBackColor = true;
+            this.btnMainMenuPanel.Click += new System.EventHandler(this.btnMainMenuPanel_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::WindowsFormsApp.Properties.Resources.Screenshot_2023_07_30_200821;
+            this.pictureBox1.Location = new System.Drawing.Point(0, -3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(125, 111);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(40)))), ((int)(((byte)(41)))));
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1080, 111);
+            this.panel1.TabIndex = 0;
+            // 
+            // pnlMain
+            // 
+            this.pnlMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlMain.Location = new System.Drawing.Point(125, 111);
+            this.pnlMain.Name = "pnlMain";
+            this.pnlMain.Size = new System.Drawing.Size(955, 596);
+            this.pnlMain.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1064, 582);
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(103)))), ((int)(((byte)(122)))));
+            this.ClientSize = new System.Drawing.Size(1080, 707);
+            this.Controls.Add(this.pnlMain);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.IsMdiContainer = true;
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+        private Panel panel2;
+        private Button btnMainMenuPanel;
+        private Button btnECommercePanel;
+        private Button btnSetSalePanel;
+        private Button btnProductCrudPanel;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Panel pnlMain;
     }
 }
--- WindowsFormsApp/Form1.cs
+++ WindowsFormsApp/Form1.cs
@@ -2,6 +2,7 @@ using Business.Abstract;
 using Business.Concrete;
 using DataAccess.Concrete.EntityFramework;
 using Entities.Concrete;
+using WindowsFormsApp.Forms;
 
 namespace WindowsFormsApp
 {
@@ -14,5 +15,38 @@ namespace WindowsFormsApp
             _iProductService = new ProductManager(new EfProductDal());
         }
 
+        void getForm(Form form,Panel panel)
+        {
+            panel.Controls.Clear();
+            form.MdiParent = this;
+            form.FormBorderStyle = FormBorderStyle.None;
+            form.Dock = DockStyle.Fill;
+            panel.Controls.Add(form);
+            form.Show();
+        }
+
+        private void btnMainMenuPanel_Click(object sender, EventArgs e)
+        {
+            MainMenuForm mainMenu = new MainMenuForm();
+            getForm(mainMenu, pnlMain);
+        }
+
+        private void btnProductCrudPanel_Click(object sender, EventArgs e)
+        {
+            ProductCrudForm productCrudForm = new ProductCrudForm();
+            getForm(productCrudForm,pnlMain);
+        }
+
+        private void btnSetSalePanel_Click(object sender, EventArgs e)
+        {
+            SetSaleForm setSaleForm = new SetSaleForm();
+            getForm(setSaleForm, pnlMain);
+        }
+
+        private void btnECommercePanel_Click(object sender, EventArgs e)
+        {
+            ECommerceForm eCommerceForm = new ECommerceForm();
+            getForm(eCommerceForm,pnlMain);
+        }
     }
 }
